cbc32b524de6d271ccc9d7529cc52cb65a46d56a
Teo Podobnik <teo.podobnik1234@gmail.com>
2022-08-29 11:31:40 +0200

    feat: Scraper works with all A1 teams

--- players.py
+++ players.py
@@ -28,7 +28,115 @@ calcit = [
     'Beravs',
     'Plot',
     'Mujanović',
-    'Pavlović',
+    'Pavlović'
 ]
 
-players_list = maribor + calcit
+panvita = [
+    'Vori',
+    'Vinkovič',
+    'McConnell',
+    'Drvarič',
+    'Vrabl',
+    'Radović',
+    'Rojnik',
+    'Flisar',
+    'Lakner',
+    'Bedrač',
+    'Fužir',
+    'Novak',
+    'Hafner',
+    'Grabar'
+]
+
+ach = [
+    'Pernuš',
+    'Šket',
+    'Rutar',
+    'Mašulović',
+    'Todorović',
+    'Peruničić',
+    'Okroglič',
+    'Kovačič',
+    'Gjorgiev',
+    'Pokeršnik',
+    'Videčnik',
+    'Kök'
+]
+
+triglav = [
+    'Ibrišimovič',
+    'Pušnik',
+    'Nanut',
+    'Tučen',
+    'Bregant',
+    'Godnič',
+    'Libnik',
+    'Snedec',
+    'Mali',
+    'Štembergar',
+    'Šikanič',
+    'Kumer'
+]
+
+crnuce = [
+    'Mihelčič',
+    'Prevočnik',
+    'Vilar',
+    'Gornik',
+    'Tepič',
+    'Rojnik',
+    'Bošnjak',
+    'Sešek',
+    'Oman',
+    'Slovnik',
+    'Jančič',
+    'Hodžić',
+    'Mejal'
+]
+
+krka = [
+    'Hafner',
+    'Erpič',
+    'Čopi',
+    'Borin',
+    'Travnik',
+    'Kožar',
+    'Pulko',
+    'Lavrič',
+    'Peterlin',
+    'Hvala',
+    'Lindič',
+    'Jaklič',
+    'Renko',
+    'Koncilja'
+]
+
+salonit = [
+    'Boharev',
+    'Stojanovič',
+    'Šarič',
+    'Jereb',
+    'Filiputti',
+    'Karnel',
+    'Lazar',
+    'Kavčič',
+    'Maretič',
+    'Radosavljević',
+    'Okroglič',
+    'Kovačič',
+    'Vrtovec',
+    'Ristić'
+]
+
+players_list = maribor + calcit + panvita + ach + triglav + crnuce + krka + salonit
+three_names_list = [
+    'Kržič',
+    'Siirila',
+    'Štembergar',
+    'Jančič',
+    'Hodžić',
+    'Maretič',
+]
+four_names_list = [
+    'Ikhbayri',
+]
--- scraper.py
+++ scraper.py
@@ -1,11 +1,7 @@
+import os
 import PyPDF2 
-from players import players_list
+from players import players_list, three_names_list, four_names_list
  
-pdfFileObj = open('cal-mb.pdf', 'rb') 
- 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-pageObj = pdfReader.getPage(0) 
-
 def rem_every_nth(lst, n):
     del lst[n-1::n]
     return lst
@@ -25,50 +21,82 @@ def rem_char(plist):
         out.append(p)
     return out
 
-#print(pageObj.extractText()) 
-all = pageObj.extractText()
-all = all.split(' ')
-all = rem_char(all)
-all = rem_emp(all)
-#print(all)
+def split100(plist):
+    out = []
+    for p in plist:
+        if '100%' in p:
+            p = p.replace('100%', '')
+            if p != '':
+                out.append(p)
+            out.append('100%')
+        else:
+            out.append(p)
+    return out
 
-STATLEN = 20
-players = []
-for t in all:
-    ok = False
-    for p in players_list:
-        if p in t and not ok:
-            players.append([p, all.index(t)])
-            ok = True
+'''
+MAIN FUNCTION
+'''
+if __name__ == '__main__':
+    DIRECTORY = 'stats'
+    for f in os.listdir(DIRECTORY):
+        path = os.path.join(DIRECTORY, f)
+        pdfFileObj = open(path, 'rb') 
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        pageObj = pdfReader.getPage(0) 
 
-# extract stats
-stats = {}
-for p,i in players:
-    stats[p] = all[(i+2):(i+STATLEN)]
+        #print(pageObj.extractText()) 
+        all = pageObj.extractText()
+        all = all.split(' ')
+        all = rem_char(all)
+        all = rem_emp(all)
+        all = split100(all)
+        #print(all)
 
-def cl(values):
-    outv = []
-    for v in values:
-        if '\n' in v:
-            s = v.split('\n')
-            #v = v.replace('\n', '')
-            outv.append(s[0])
+        STATLEN = 20
+        players = []
+        for t in all:
+            ok = False
+            for p in players_list:
+                if p in t and not ok:
+                    players.append([p, all.index(t)])
+                    ok = True
+
+        # extract stats
+        stats = {}
+        for p,i in players:
+            a = 0
+            # Filter longer names
+            if p in three_names_list:
+                a = 1
+            if p in four_names_list:
+                a = 2
+            stats[p] = all[(i+a+2):(i+a+STATLEN)]
+
+        def cl(values):
+            outv = []
+            for v in values:
+                if '\n' in v:
+                    s = v.split('\n')
+                    #v = v.replace('\n', '')
+                    outv.append(s[0])
+                    return outv
+                outv.append(v)
             return outv
-        outv.append(v)
-    return outv
 
-# TODO: fix the one with longer names
 
-# Clean stats
-cstats = {}
-for k,v in stats.items():
-    cstats[k] = cl(v)
+        # Clean stats
+        cstats = {}
+        for k,v in stats.items():
+            cstats[k] = cl(v)
 
-for k,vs in cstats.items():
-    if vs[0].isnumeric():
-        vs.pop(0)
+        for k,vs in cstats.items():
+            if vs[0].isnumeric():
+                vs.pop(0)
 
-print(cstats)
+        #print(cstats)
+        for k,v in cstats.items():
+            print(k, v)
+            assert len(v) == 17
 
- 
-pdfFileObj.close()
+         
+        pdfFileObj.close()
