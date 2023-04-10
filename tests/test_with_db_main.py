ffc243913f7e4b681ddd493a1091c7935e81f6a9
Teodor Janez Podobnik <teo.podobnik1234@gmail.com>
2023-04-10 12:30:37 +0200

    wokring

--- tests/test_with_db_main.py
+++ tests/test_with_db_main.py
@@ -3,85 +3,8 @@ import sys
 import unittest
 import requests
 from voliboli_pdf_scraper.main import process_pdf
-
-from sgqlc.types import String, Type, Field, ID, Boolean, list_of
+from voliboli_sgqlc_types.main import Mutation
 from sgqlc.operation import Operation
-
-class Player(Type):
-    name = String
-    teamName = String
-    votes = String
-    totalPoints = String
-    breakPoints = String
-    winloss = String
-    totalServe = String
-    errorServe = String
-    pointsServe = String
-    totalReception = String
-    errorReception = String
-    posReception = String
-    excReception = String
-    totalAttacks = String
-    errorAttacks = String
-    blockedAttacks = String
-    pointsAttack = String
-    posAttack = String
-    pointsBlock = String
-    dateTeam = String
-
-class PlayerResult(Type):
-    success = Boolean
-    errors =  list_of(String)
-    player = Player
-
-class PlayersResult(Type):
-    success = Boolean
-    errors = list_of(String)
-    players = list_of(Player)
-
-class Team(Type):
-    name = String
-    players = list_of(Player)
-
-class TeamResult(Type):
-    success = Boolean
-    errors = list_of(String)
-    team = Team
-
-class TeamsResult(Type):
-    success = Boolean
-    errors = list_of(String)
-    teams = list_of(Team)
-
-class Query(Type):
-    getTeams = Field(TeamsResult)
-    getPlayer = Field(PlayerResult, args={'name': String})
-    getPlayers = Field(PlayersResult)
-
-class Mutation(Type):
-    createTeam = Field(TeamResult, args={'name': String})
-    deleteTeam = Field(TeamResult, args={'name': String})
-    createPlayer = Field(PlayerResult, args={'name': String, 'teamName': String})
-    updatePlayer = Field(PlayerResult, args={'name': String,
-                                             'votes': String, 
-                                             'totalPoints': String,
-                                             'breakPoints': String,
-                                             'winloss': String,
-                                             'totalServe': String,
-                                             'errorServe': String,
-                                             'pointsServe': String,
-                                             'totalReception': String,
-                                             'errorReception': String,
-                                             'posReception': String,
-                                             'excReception': String,
-                                             'totalAttacks': String,
-                                             'errorAttacks': String,
-                                             'blockedAttacks': String,
-                                             'pointsAttack': String,
-                                             'posAttack': String,
-                                             'pointsBlock': String,
-                                             'dateTeam': String})
-    deletePlayer = Field(PlayerResult, args={'name': String})
 
 class TestPDFScraper(unittest.TestCase):
     STAT_DIRECTORY = 'stats'
@@ -89,43 +12,42 @@ class TestPDFScraper(unittest.TestCase):
 
     def setUp(self):
         self.BASE = "http://172.34.1.3:5000"
-        self.query = Operation(Query)
 
     def store_data(self, team, players, date):
-        self.mutation = Operation(Mutation)
-        self.mutation.createTeam(name=team)
-        resp = requests.post(self.BASE + "/teams", json={'query': str(self.mutation)})
+        mutation = Operation(Mutation)
+        mutation.createTeam(name=team)
+        resp = requests.post(self.BASE + "/teams", json={'query': str(mutation)})
         if not resp.json()["data"]["createTeam"]["success"]:
             print(resp.json()["data"]["createTeam"]["errors"])
 
         for p in players:
-            self.mutation = Operation(Mutation)
-            self.mutation.createPlayer(name=p[0], teamName=team)
-            resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
+            mutation = Operation(Mutation)
+            mutation.createPlayer(name=p[0], teamName=team)
+            resp = requests.post(self.BASE + "/players", json={'query': str(mutation)})
             if not resp.json()["data"]["createPlayer"]["success"]:
                 print(resp.json()["data"]["createPlayer"]["errors"])
                 
-            self.mutation = Operation(Mutation)
-            self.mutation.updatePlayer(name=p[0],
-                                       votes=p[1], 
-                                       totalPoints=p[2],
-                                       breakPoints=p[3],
-                                       winloss=p[4],
-                                       totalServe=p[5],
-                                       errorServe=p[6],
-                                       pointsServe=p[7],
-                                       totalReception=p[8],
-                                       errorReception=p[9],
-                                       posReception=p[10],
-                                       excReception=p[11],
-                                       totalAttacks=p[12],
-                                       errorAttacks=p[13],
-                                       blockedAttacks=p[14],
-                                       pointsAttack=p[15],
-                                       posAttack=p[16],
-                                       pointsBlock=p[17],
-                                       dateTeam=date)
-            resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
+            mutation = Operation(Mutation)
+            mutation.updatePlayer(name=p[0],
+                                    votes=p[1], 
+                                    totalPoints=p[2],
+                                    breakPoints=p[3],
+                                    winloss=p[4],
+                                    totalServe=p[5],
+                                    errorServe=p[6],
+                                    pointsServe=p[7],
+                                    totalReception=p[8],
+                                    errorReception=p[9],
+                                    posReception=p[10],
+                                    excReception=p[11],
+                                    totalAttacks=p[12],
+                                    errorAttacks=p[13],
+                                    blockedAttacks=p[14],
+                                    pointsAttack=p[15],
+                                    posAttack=p[16],
+                                    pointsBlock=p[17],
+                                    dateTeam=date)
+            resp = requests.post(self.BASE + "/players", json={'query': str(mutation)})
             if not resp.json()["data"]["updatePlayer"]["success"]:
                 sys.exit(resp.json()["data"]["updatePlayer"]["errors"])
                 
